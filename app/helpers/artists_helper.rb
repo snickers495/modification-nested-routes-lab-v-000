77ae68652a96bc6760eaeed21c1fb0e1d6487d5b
Lisa Lee <snickers495@gmail.com>
2017-12-07 17:46:31 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,11 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
-  def artist_select 
-
+  def artist_select(song)
+    if song.artist.nil?
+      select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+    else
+      hidden_field_tag "post[author_id]", post.author_id
+    end
   end
 end
