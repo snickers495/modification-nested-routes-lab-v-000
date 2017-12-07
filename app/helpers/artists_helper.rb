bb622d25027c7771859c06cab7cd780b924c1c8d
Lisa Lee <snickers495@gmail.com>
2017-12-07 17:46:45 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,7 +5,7 @@ module ArtistsHelper
 
   def artist_select(song)
     if song.artist.nil?
-      select_tag "song[artist_id]", options_from_collection_for_select(Author.all, :id, :name)
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
       hidden_field_tag "post[author_id]", post.author_id
     end
