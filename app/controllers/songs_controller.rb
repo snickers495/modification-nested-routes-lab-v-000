087a9ea2b342473e0fa9068166d27d3925e4d9df
Lisa Lee <snickers495@gmail.com>
2017-12-07 18:03:27 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -51,7 +51,7 @@ class SongsController < ApplicationController
       else
         artist = Artist.find_by(id: params[:artist_id])
         @song = artist.songs.find_by(id: params[:id])
-        redirect_to artist_path(artist), alert: "Song not found." if @song.nil?
+        redirect_to artist_songs_path(artist), alert: "Song not found." if @song.nil?
       end
     else
       @song = Song.find(params[:id])
